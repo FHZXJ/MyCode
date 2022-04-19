21c8d6df330163ffc1a24c5e7141911dd9932c11
‘fanhuan’ <fanhuan@succez.com>
2022-04-19 16:26:20 +0800

    test

---
+++ src/com/fan/java/test.java
@@ -0,0 +1,11 @@
+package com.fan.java;
+
+/**
+ * @author AIZXJ
+ * @create 2022-04-19 16:22
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
